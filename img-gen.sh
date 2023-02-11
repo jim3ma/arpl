718a41edb40c5ccd26fffc55596ebfc6daf5e529
Jim Ma <majinjing3@gmail.com>
2023-02-11 17:15:40 +0800

    update image build

--- img-gen.sh
+++ img-gen.sh
@@ -14,16 +14,16 @@ rm -rf ".buildroot/board/arpl/p3"
 
 # Get latest LKMs
 echo "Getting latest LKMs"
-if [ `ls ../redpill-lkm/output | wc -l` -eq 0 ]; then
+if [ `ls ../redpill-lkm5/output | wc -l` -eq 0 ]; then
   echo "  Downloading from github"
   TAG=`curl -s https://api.github.com/repos/fbelavenuto/redpill-lkm/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3)}'`
   curl -L "https://github.com/fbelavenuto/redpill-lkm/releases/download/${TAG}/rp-lkms.zip" -o /tmp/rp-lkms.zip
   rm -rf files/board/arpl/p3/lkms/*
   unzip /tmp/rp-lkms.zip -d files/board/arpl/p3/lkms
 else
-  echo "  Copying from ../redpill-lkm/output"
+  echo "  Copying from ../redpill-lkm5/output"
   rm -rf files/board/arpl/p3/lkms/*
-  cp -f ../redpill-lkm/output/* files/board/arpl/p3/lkms
+  cp -f ../redpill-lkm5/output/* files/board/arpl/p3/lkms
 fi
 
 # Get latest addons and install its
@@ -81,13 +81,13 @@ echo "Version: ${VERSION}"
 echo "Building... Drink a coffee and wait!"
 make BR2_EXTERNAL=../external -j`nproc`
 cd -
-qemu-img convert -O vmdk arpl.img arpl-dyn.vmdk
-qemu-img convert -O vmdk -o adapter_type=lsilogic arpl.img -o subformat=monolithicFlat arpl.vmdk
+# qemu-img convert -O vmdk arpl.img arpl-dyn.vmdk
+# qemu-img convert -O vmdk -o adapter_type=lsilogic arpl.img -o subformat=monolithicFlat arpl.vmdk
 [ -x test.sh ] && ./test.sh
 rm -f *.zip
 zip -9 "arpl-${VERSION}.img.zip" arpl.img
-zip -9 "arpl-${VERSION}.vmdk-dyn.zip" arpl-dyn.vmdk
-zip -9 "arpl-${VERSION}.vmdk-flat.zip" arpl.vmdk arpl-flat.vmdk
+# zip -9 "arpl-${VERSION}.vmdk-dyn.zip" arpl-dyn.vmdk
+# zip -9 "arpl-${VERSION}.vmdk-flat.zip" arpl.vmdk arpl-flat.vmdk
 sha256sum update-list.yml > sha256sum
 zip -9j update.zip update-list.yml
 while read F; do
